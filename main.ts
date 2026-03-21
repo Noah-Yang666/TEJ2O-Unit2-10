d17fae57cbaa5449ef5ddcac15c2763e35070161
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-21 14:17:21 -0400

    programmed neopixels to light up depending on temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah Yang
+ * Created on: March 2026
+ * This program turns neopixels on depending on the lighting
 */
 
-basic.showString('Hello, World!')
+// variables needed
+let lightLevel: number = 0
+const myNeopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+// setting up
+basic.showIcon(IconNames.Happy)
+myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+
+// simulating code for lighting up neopixels
+input.onButtonPressed(Button.A, function () {
+    myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    if (lightLevel > 52) {
+        myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevel > 104) {
+        myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevel > 156) {
+        myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (lightLevel > 208) {
+        myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    }
+    myNeopixelStrip.show()
+})
