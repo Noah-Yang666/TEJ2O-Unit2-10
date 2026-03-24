b9591bfaa5b0a6d77de3a7b77bb2cae77722b9bc
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-24 13:09:03 -0400

    fixed errors and perfected code

--- main.ts
+++ main.ts
@@ -5,46 +5,39 @@
  * This program turns neopixels on depending on the lighting
 */
 
-// variables needed
+//variables
 let lightLevel: number = 0
 const myNeopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
-
-// setup
+//setup
 basic.showIcon(IconNames.Happy)
 myNeopixelStrip.clear()
 myNeopixelStrip.show()
-basic.clearScreen()
 
-// running Button A
+// code for lighting up neopixels
 input.onButtonPressed(Button.A, function () {
-    basic.pause(1000)
     lightLevel = input.lightLevel()
-    basic.showNumber(lightLevel)
 
+    // shows the current light level 
+    basic.showNumber(lightLevel)
     myNeopixelStrip.clear()
+
     if (lightLevel > 52) {
         myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     }
+
     if (lightLevel > 104) {
         myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
     }
+
     if (lightLevel > 156) {
         myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     }
+
     if (lightLevel > 208) {
         myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
     }
 
     myNeopixelStrip.show()
-
-    basic.showString("Light level is " + lightLevel)
-})
-
-// running Button B
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
     basic.showNumber(lightLevel)
-    myNeopixelStrip.clear()
-    myNeopixelStrip.show()
 })
